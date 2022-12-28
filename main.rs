6c3c0b322005f1f5580e38b0cf67b1d02e697899
Italo Andrade <italoandradejr@gmail.com>
2022-12-28 17:37:03 -0300

    primeiros passos com rust

--- main.rs
+++ main.rs
@@ -1,3 +1,13 @@
 fn main () {
-	println!("mundo")
+	let variavel:u8 = 128;
+	println!("variavel = {}, tamanho = {} bytes", variavel, std::mem::size_of_val(&variavel));
+	
+	let decimal:f32 = 2.5;
+	println!("decimal = {}", decimal);
+
+	let mut booleana:bool = true;
+	println!("Booleana = {}, tamanho booleana = {} bytes", booleana, std::mem::size_of_val(&booleana));
+
+	let letra:str = "C";
+	println!("Tamanho do char = {} bytes", std::mem::size_of_val(&letra));
 }
