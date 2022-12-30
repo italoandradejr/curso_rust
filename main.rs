64c02e38244346a7f101cf11e35f632d2cb69980
Italo Andrade <italoandradejr@gmail.com>
2022-12-30 17:50:39 -0300

    atualização rust

--- main.rs
+++ main.rs
@@ -1,5 +1,31 @@
-fn main () {
-	let variavel:u8 = 128;
+const PI:f32 = 3.14;
+static mut GLOBAL:u8 = 1;
+
+
+fn soma (a:i32, b:i32) -> i32 {
+	println! ("{} + {} = {}", a, b, a + b);
+	a + b
+}
+
+
+fn sombra () {
+	let a = 12;
+
+	{
+		let b = 456;
+		println! ("dentro, b = {}", b);
+	}
+}
+
+fn escopo () {
+
+println!("PI = {}", PI);
+
+	unsafe {
+	println!("variavel_global = {}", GLOBAL);
+}
+
+	let variavel:i32 = 300;
 	println!("variavel = {}, tamanho = {} bytes", variavel, std::mem::size_of_val(&variavel));
 	
 	let decimal:f32 = 2.5;
@@ -8,6 +34,14 @@ fn main () {
 	let mut booleana:bool = true;
 	println!("Booleana = {}, tamanho booleana = {} bytes", booleana, std::mem::size_of_val(&booleana));
 
-	let letra:str = "C";
+	let letra:&str = "C";
 	println!("Tamanho do char = {} bytes", std::mem::size_of_val(&letra));
+}
+
+
+fn main () {
+	escopo();	
+	sombra();
+	println!("soma = {}", soma(2,2));
+	
 }
