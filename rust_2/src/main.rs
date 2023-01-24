2a6b2f4252ae2fcd2209cafeff38f57cdf3644c4
Italo Andrade <italoandradejr@gmail.com>
2023-01-24 17:38:58 -0300

    curso de rust 24-01

--- rust_2/src/main.rs
+++ rust_2/src/main.rs
@@ -7,6 +7,11 @@ fn main () {
     }
 
     matriz ();
+    println!("É fim de de semana? {}", eh_fim_de_semana(DiaDaSemana::Quarta));
+
+    cores ();
+
+    conteudo_opcional ()
 }
 
 fn matriz () {
@@ -20,4 +25,61 @@ fn matriz () {
             println!("Item = {}", item);
         }
     }
+}
+
+#[allow(dead_code)]
+enum DiaDaSemana { 
+    Domingo,
+    Segunda,
+    Terca,
+    Quarta,
+    Quinta,
+    Sexta,
+    Sabado
+}
+
+fn eh_fim_de_semana(dia_da_semana: DiaDaSemana) -> bool
+{
+    match dia_da_semana { 
+        DiaDaSemana::Domingo | DiaDaSemana::Sabado => true, 
+        _ => false 
+    }
+}
+
+#[allow(dead_code)]
+enum Color {
+    Red,
+    Green,
+    Blue,
+    RgbColor(u8, u8, u8),
+    CymkColor{cyan: u8, magenta: u8, yellow: u8, black:u8}
+}
+
+fn cores () {
+    let cor = Color::CymkColor{cyan: 100, magenta: 50, yellow: 70, black:255};
+
+    println!("Cor = {}", match cor {
+        Color:: Red => "Vermelho",
+        Color:: Green => "Verde",
+        Color::Blue => "blue",
+        Color::RgbColor(0,0,0)
+            | Color::CymkColor{cyan:_,magenta:_,yellow:_,black:255} => "Preta",
+        Color::RgbColor(_,_,_) => "RGB desconhecido",
+        Color::CymkColor{cyan:_,magenta:_,yellow:_,black:_} => "CYMK desconhecido"
+    });
+}
+
+fn conteudo_opcional () {
+    let conteudo_arquivo = ler_arquivo(String::from(""));
+
+    match &conteudo_arquivo {
+        Some (valor) => println!("{}", valor),
+        None => println! ("ARquivo não existe")
+    };
+
+    println!("{:?}", conteudo_arquivo);
+}
+
+fn ler_arquivo(caminho_arquivo: String) -> Option<String> {
+    Some(String::from("Conteudo do arquivo"))
 }
